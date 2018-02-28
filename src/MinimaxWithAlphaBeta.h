7c9c1b56c13317bfa29317829b1c0090c6b379e8
Jason Warta <jasonwarta@gmail.com>
2018-02-27 16:25:24 -0900

    initial alphabeta

---
+++ src/MinimaxWithAlphaBeta.h
@@ -0,0 +1,124 @@
+// MinimaxWithAlphaBeta.h
+
+#ifndef MINIMAX_WITH_ALPHA_BETA_H_INCLUDED
+#define MINIMAX_WITH_ALPHA_BETA_H_INCLUDED
+
+#include <string>
+#include <algorithm>
+#include "checkers.h"
+#include "BasicBoardEval.h"
+
+class MinimaxWithAlphaBeta {
+public:
+	MinimaxWithAlphaBeta(std::string &theBoard, int depth, bool redPlayer) {
+		redPlayerTurn_ = redPlayer;
+		breakAlpha_ = 0;
+		breakBeta_ = 0;
+
+		CheckerBoard tempBoard(theBoard, redPlayerTurn_);
+		std::vector< std::string > possBoards = std::move(tempBoard.getAllRandoMoves());
+
+		if(possBoards.size() == 0){
+			bestBoard_ = theBoard;
+			return;
+		}
+
+		int alpha = -10000;
+		int beta = 10000;
+
+		int bestVal = minimaxWithAlphaBetaRecursive(possBoards[0], depth-1, alpha, beta, true);
+		bestBoard_ = possBoards[0];
+
+		for(auto it = possBoards.begin()+1; it != possBoards.end(); ++it) {
+			int val = minimaxWithAlphaBetaRecursive(*it, depth-1, alpha, beta, true);
+			if ( val > bestVal ) {
+				bestVal = val;
+				bestBoard_ = *it;
+			}
+		}
+	}
+
+	std::string getBestBoard() {
+		if( !breakAlpha_ || !breakBeta_ )
+			std::cout << "alpha: " << breakAlpha_ << ", beta: " << breakBeta_ << std::endl;
+		return bestBoard_;
+	}
+
+private:
+	int minimaxWithAlphaBetaRecursive(std::string &theBoard, int depth, int alpha, int beta, bool maximizingPlayer) {
+		if(depth == 0) {
+			return basicBoardEval(theBoard, redPlayerTurn_);
+		}
+
+		if (maximizingPlayer) {
+			CheckerBoard tempBoard(theBoard, redPlayerTurn_);
+			std::vector<std::string> possBoards = std::move(tempBoard.getAllRandoMoves());
+
+			int bestVal = -10000;
+
+			if(possBoards.size() == 0)
+				return bestVal;
+
+			for(auto it = possBoards.begin(); it != possBoards.end(); ++it) {
+				bestVal = std::max(bestVal, minimaxWithAlphaBetaRecursive(*it, depth-1, alpha, beta, false));
+				alpha = std::max(alpha,bestVal);
+				if(beta <= alpha) {
+					// std::cout << "beta" << std::endl;
+					breakBeta_++;
+					break;
+				}
+			}
+			return bestVal;
+		}
+		else {
+			CheckerBoard tempBoard(theBoard, !redPlayerTurn_);
+			std::vector<std::string> possBoards = std::move(tempBoard.getAllRandoMoves());
+
+			int worstVal = 10000;
+
+			if(possBoards.size() == 0)
+				return worstVal;
+
+			for(auto it = possBoards.begin(); it != possBoards.end(); ++it) {
+				worstVal = std::min(worstVal, minimaxWithAlphaBetaRecursive(*it, depth-1, alpha, beta, true));
+				beta = std::min(beta,worstVal);
+				if(beta <= alpha) {
+					breakAlpha_++;
+					// std::cout << "alpha" << std::endl;
+					break;
+				}
+			}
+			return worstVal;
+		}
+	}
+
+
+// 01 function alphabeta(node, depth, α, β, maximizingPlayer)
+// 02      if depth = 0 or node is a terminal node
+// 03          return the heuristic value of node
+// 04      if maximizingPlayer
+// 05          v := -∞
+// 06          for each child of node
+// 07              v := max(v, alphabeta(child, depth – 1, α, β, FALSE))
+// 08              α := max(α, v)
+// 09              if β ≤ α
+// 10                  break (* β cut-off *)
+// 11          return v
+// 12      else
+// 13          v := +∞
+// 14          for each child of node
+// 15              v := min(v, alphabeta(child, depth – 1, α, β, TRUE))
+// 16              β := min(β, v)
+// 17              if β ≤ α
+// 18                  break (* α cut-off *)
+// 19          return v
+// (* Initial call *)
+// alphabeta(origin, depth, -∞, +∞, TRUE)
+private:
+	std::string bestBoard_;
+	bool redPlayerTurn_;
+	int breakBeta_;
+	int breakAlpha_;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "consts.h"
 // #include "checkers.h"
 #include "MinMaxTree.h"
+#include "MinimaxWithAlphaBeta.h"
 
 // void launchServer(Communicator * comm) {
 // 	uWS::Hub h;
@@ -47,44 +48,61 @@ int main(int argc, char const *argv[]) {
 		// values assignments are not backed up by math or statistics, and could
 		// probably be refined to some extent, especially once we improve the BEF
 		if( redTeamTurn ) {
-			if(turnCounter > 120 ) {
-				MinMaxTree board(boardString,8,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
-			else if(turnCounter > 80 && totalPieceCount(boardString) < 8 ) {
-				MinMaxTree board(boardString,8,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
-			else if( totalPieceCount(boardString) < 4 ) {
-				MinMaxTree board(boardString,8,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
-			else if ( totalPieceCount(boardString) < 6 ) {
-				MinMaxTree board(boardString,7,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
-			else if ( totalPieceCount(boardString) < 8 ) {
-				MinMaxTree board(boardString,6,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
-			else if ( totalPieceCount(boardString) < 12 ) {
-				MinMaxTree board(boardString,5,redTeamTurn);
-				boardString = board.getBestBoard();
-			} 
-			else {
-				MinMaxTree board(boardString,4,redTeamTurn);
-				boardString = board.getBestBoard();
-			}
+			// if(turnCounter > 120 ) {
+			// 	MinMaxTree board(boardString,8,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			// else if(turnCounter > 80 && totalPieceCount(boardString) < 8 ) {
+			// 	MinMaxTree board(boardString,8,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			// else if( totalPieceCount(boardString) < 4 ) {
+			// 	MinMaxTree board(boardString,8,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			// else if ( totalPieceCount(boardString) < 6 ) {
+			// 	MinMaxTree board(boardString,7,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			// else if ( totalPieceCount(boardString) < 8 ) {
+			// 	MinMaxTree board(boardString,6,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			// else if ( totalPieceCount(boardString) < 12 ) {
+			// 	MinMaxTree board(boardString,5,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// } 
+			// else {
+			// 	MinMaxTree board(boardString,4,redTeamTurn);
+			// 	boardString = board.getBestBoard();
+			// }
+			
+			// MinMaxTree board(boardString,4,redTeamTurn);
+			// boardString = board.getBestBoard();
+
+			MinimaxWithAlphaBeta board(boardString,6,redTeamTurn);
+			boardString = board.getBestBoard();
 		}
 
 		// black team uses random move picking... should always lose
 		else {
+			// MinMaxTree board(boardString,4,redTeamTurn);
+			// boardString = board.getBestBoard();
+
 			CheckerBoard board(boardString,redTeamTurn);
 			boardString = board.getRandoMove();
 		}
 
 		if ( boardString.size() < 32) {
 			std::cout << (redTeamTurn ? "Red team" : "Black team") << " lost the game" << std::endl;
+			break;
+		} 
+		else if ( !rPieceCount(boardString) ) {
+			std::cout << "Red team lost the game" << std::endl;
+			break;
+		}
+		else if ( !bPieceCount(boardString) ) {
+			std::cout << "Black team lost the game" << std::endl;
 			break;
 		}
 
