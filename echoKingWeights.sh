4f853a120f526a8d7ef87ed42e1f5b53bf429f05
Jason Warta <jasonwarta@gmail.com>
2018-03-19 00:32:50 -0800

    added more info to king weight printout

--- echoKingWeights.sh
+++ echoKingWeights.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 for f in `ls NN`;do 
-	echo "$f"
-	for g in `ls NN/$f/nets`;do
-		w="$(cat NN/$f/nets/$g|grep -i 'kw'|sed 's/kw/kw:/' )"
-		echo -e "net: $g  \t$w"
+	echo "`sed 's/gen_/\nGeneration: /'<<<$f`"
+	SUM=0.0
+	for NET in `ls NN/$f/nets`;do
+		KW="$(cat NN/$f/nets/$NET|grep -i 'kw'|sed 's/kw //' )"
+		SUM=`echo $SUM + $KW | bc`
+		SCORE=`grep "$NET: " NN/$f/scores 2>/dev/null|sed "s/$NET: //"`
+		echo -e "net: $NET  \tkw: $KW\tScore: $SCORE"
 	done
+	echo Avg KingWeight: `echo "$SUM / 30" | bc -l`
 done
