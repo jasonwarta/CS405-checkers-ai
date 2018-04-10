cfc76e249892a95ad852be7f3f7fdc86a0606cdf
FromAlaska <jasamson@alaska.edu>
2018-04-10 12:42:49 -0800

    added more features

--- Python_Data_Visualization/main.py
+++ Python_Data_Visualization/main.py
@@ -1,5 +1,6 @@
 import json
 import matplotlib.pyplot as plt
+from pylab import figure, axes, pie, title, show
 
 json_data = open('00v01').read()
 jsonToPython = json.loads(json_data)
@@ -7,9 +8,8 @@ jsonToPython = json.loads(json_data)
 for i in range(1,22):
     print jsonToPython[i]["move"] + " " + jsonToPython[i]["time"]
     plt.plot(jsonToPython[i]["move"],jsonToPython[i]["time"],'ro')
+plt.title("NN Gen185 Game 00v01")
+plt.xlabel("Generations per evolution")
+plt.ylabel("Time it took to evolve")
+#plt.savefig("Gen185_00v01.pdf")
 plt.show()
-
-# jsonData = jsonToPython["data"]
-# for item in jsonData:
-#     name = item.get("Name")
-#     campaignID = item.get("CampaignID")
