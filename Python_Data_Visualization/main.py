e79482ee28161469c4c95f9734f2a43b405f2168
FromAlaska <Jim_samson1335@yahoo.com>
2018-04-11 09:01:26 -0800

    added features

--- Python_Data_Visualization/main.py
+++ Python_Data_Visualization/main.py
@@ -1,27 +1,49 @@
 from subprocess import check_output
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 from os import listdir
 import json
 import sys
 
-countFiles = 1;
-files = []
+def openSingleFile(file):
+    output = check_output("grep { " + file + " | sed -e 's/$/,/'",shell=True)
 
-# Open files in directory
-#for list_files in listdir('../NN_185gens/gen_185/games'):
+    jsonToPython = json.loads((b'[' + output + b'{"string":"foo"}' + b']').decode('utf-8'))
+    json_size = len(jsonToPython)
 
-file = sys.argv[1]
-output = check_output("grep { " + file + " | sed -e 's/$/,/'",shell=True)
+    for index in range(1,json_size-1):
+        print(jsonToPython[index]["move"] + " " + jsonToPython[index]["time"])
+        plt.plot(jsonToPython[index]["move"],jsonToPython[index]["time"],'ro')
 
-jsonToPython = json.loads((b'[' + output + b'{"string":"foo"}' + b']').decode('utf-8'))
-json_size = len(jsonToPython)
+def openMultipleFiles():
+    for cur_file in data_files:
+        output = check_output("grep { " + "../NN_185gens/gen_185/games/" + cur_file + " | sed -e 's/$/,/'",shell=True)
+        jsonToPython = json.loads((b'[' + output + b'{"string":"foo"}' + b']').decode('utf-8'))
+        json_size = len(jsonToPython)
 
-for i in range(1,json_size-1):
-    print(jsonToPython[i]["move"] + " " + jsonToPython[i]["time"])
-    plt.plot(jsonToPython[i]["move"],jsonToPython[i]["time"],'ro')
+        for i in range(1,json_size-1):
+            print(jsonToPython[i]["move"] + " " + jsonToPython[i]["time"])
+            plt.plot(jsonToPython[i]["move"],jsonToPython[i]["time"],'ro')
 
-# plt.title("NN Gen185 Game 00v01")
-# plt.xlabel("Generations per evolution")
-# plt.ylabel("Time it took to evolve")
-#plt.savefig("Gen185_00v01.pdf")
-plt.show()
+def main():
+
+    data_files = []
+    # Open files in directory
+    for list_files in listdir('../NN_185gens/gen_185/games'):
+        data_files.append(list_files)
+
+    if(len(sys.argv)==2):
+        openSingleFile(sys.argv[1])
+    else:
+        openMultipleFiles()
+
+    plt.title("NN Gen185 Game")
+    # plt.xlabel("Generations per evolution")
+    # plt.ylabel("Time it took to evolve")
+    #plt.savefig("Gen185_00v01.pdf")
+    style.use('fivethirtyeight')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
